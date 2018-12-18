bac52da031f0a31c9040e171ca85fdfe144956f5
萬俊瑋 <wei840222@gmail.com>
2018-12-18 23:12:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# drone-line-flex-notify-plugin
+
+```
+pipeline:
+    line-flex-notify:
+        image: wei840222/drone-line-flex-notify-plugin
+        access_token: chanel accessToken
+        to: groupId
+```
---
+++ main.js
@@ -0,0 +1,40 @@
+(async function () {
+    const axios = require('axios')
+    const flex = require('./flex.json')
+
+    const {
+        DRONE_REPO_NAME,
+        DRONE_COMMIT_MESSAGE,
+        DRONE_COMMIT_BRANCH,
+        DRONE_BUILD_EVENT,
+        DRONE_COMMIT_AUTHOR,
+        CI_BUILD_NUMBER,
+        DRONE_BUILD_STATUS,
+        DRONE_COMMIT_LINK,
+        PLUGIN_ACCESS_TOKEN,
+        PLUGIN_TO
+    } = process.env
+
+    flex.contents.body.contents[1].text = DRONE_REPO_NAME
+    flex.contents.body.contents[2].text = DRONE_COMMIT_MESSAGE
+    flex.contents.body.contents[4].contents[0].contents[1].text = DRONE_COMMIT_BRANCH
+    flex.contents.body.contents[4].contents[1].contents[1].text = DRONE_BUILD_EVENT
+    flex.contents.body.contents[4].contents[2].contents[1].text = DRONE_COMMIT_AUTHOR
+    flex.contents.body.contents[4].contents[3].contents[1].text = CI_BUILD_NUMBER
+    flex.contents.body.contents[4].contents[4].contents[1].text = DRONE_BUILD_STATUS
+    flex.contents.body.contents[5].contents[0].action.uri = DRONE_COMMIT_LINK
+
+    await axios.post('https://api.line.me/v2/bot/message/push',
+        {
+            'to': PLUGIN_TO,
+            'messages': [ flex ]
+        },
+        {
+            headers:
+            {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${PLUGIN_ACCESS_TOKEN}`
+            }
+        }
+    ).catch(err => console.log(err))
+})()

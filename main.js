0bb6ee0425fcad67420a0bfb221f2a55ab9736ba
wei-wan <wei.wan@linecorp.com>
2019-04-10 09:37:45 +0800

    support drone 1.0.0

--- main.js
+++ main.js
@@ -12,7 +12,8 @@
         DRONE_BUILD_STATUS,
         DRONE_COMMIT_LINK,
         PLUGIN_ACCESS_TOKEN,
-        PLUGIN_TO
+        PLUGIN_TO,
+        DRONE_BUILD_NUMBER
     } = process.env
 
     flex.contents.body.contents[1].text = DRONE_REPO_NAME
@@ -20,8 +21,9 @@
     flex.contents.body.contents[4].contents[0].contents[1].text = DRONE_COMMIT_BRANCH
     flex.contents.body.contents[4].contents[1].contents[1].text = DRONE_BUILD_EVENT
     flex.contents.body.contents[4].contents[2].contents[1].text = DRONE_COMMIT_AUTHOR
-    flex.contents.body.contents[4].contents[3].contents[1].text = CI_BUILD_NUMBER
+    flex.contents.body.contents[4].contents[3].contents[1].text = CI_BUILD_NUMBER || DRONE_BUILD_NUMBER
     flex.contents.body.contents[4].contents[4].contents[1].text = DRONE_BUILD_STATUS
+    flex.contents.body.contents[4].contents[4].contents[1].color = DRONE_BUILD_STATUS === 'success' ? '#33aa55' : '#cc3d33'
     flex.contents.body.contents[5].contents[0].action.uri = DRONE_COMMIT_LINK
 
     await axios.post('https://api.line.me/v2/bot/message/push',
